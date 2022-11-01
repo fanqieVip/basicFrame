7f8ef96539c08464f1e3b9d15f6dbc87f09a4037
fanqieVip <42194904+fanqieVip@users.noreply.github.com>
2022-11-01 18:38:05 +0800

    Update WatchKtx.kt
    优化Watch语法，修复内存泄漏风险

--- lib_base/src/main/java/com/frame/basic/base/ktx/WatchKtx.kt
+++ lib_base/src/main/java/com/frame/basic/base/ktx/WatchKtx.kt
@@ -1,77 +1,39 @@
 package com.frame.basic.base.ktx
 
-import android.app.Activity
-import androidx.fragment.app.Fragment
-import androidx.lifecycle.*
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Observer
+import com.frame.basic.base.mvvm.vm.BaseVM
+import kotlin.collections.ArrayList
+import kotlin.collections.set
 
 /**
- * LiveData观察者，可自定义观察对象，及时更新自己
+ * 绑定生命周期的Watch方法
  */
-class WatchLiveData<T>(initData: T? = null) : MutableLiveData<T>() {
-    init {
-        if (initData != null){
-            postValue(initData)
+fun <T, DATA> MutableLiveData<T>.watch(
+    vm: BaseVM,
+    data: MutableLiveData<DATA>?,
+    onChange: (data: DATA?, self: MutableLiveData<T>) -> Unit
+): MutableLiveData<T> {
+    data?.let {
+        var watchData = vm.watchLiveDataMap[it]
+        if (watchData == null) {
+            watchData = ArrayList<Observer<*>>()
         }
+        watchData.add(Observer<DATA> { t -> onChange.invoke(t, this) })
+        vm.watchLiveDataMap[it] = watchData
     }
-    private val watchData by lazy { HashMap<MutableLiveData<*>, Observer<*>>() }
+    return this
+}
 
-    /**
-     * 该方法会绑定生命周期自动绑定和解绑
-     */
-    fun <D> watch(
-        data: MutableLiveData<D>?,
-        onChange: (data: D?, self: WatchLiveData<T>) -> Unit
-    ): WatchLiveData<T> {
-        data?.let {
-            watchData.put(it, Observer<D> { t ->
-                onChange.invoke(t, this@WatchLiveData) }
-            )
-        }
-        return this
+/**
+ * 不绑定生命周期的Watch方法，有内存泄漏风险，谨慎使用
+ */
+fun <T, DATA> MutableLiveData<T>.watchForever(
+    data: MutableLiveData<DATA>?,
+    onChange: (data: DATA?, self: MutableLiveData<T>) -> Unit
+): MutableLiveData<T> {
+    data?.observeForever {
+        onChange(it, this)
     }
-
-    /**
-     * 该方法不绑定生命周期，容易造成内存泄漏，谨慎使用
-     */
-    fun <D> watchForever(
-        data: MutableLiveData<D>?,
-        onChange: (data: D?, self: WatchLiveData<T>) -> Unit
-    ): WatchLiveData<T> {
-        data?.observeForever {
-            onChange(it, this)
-        }
-        return this
-    }
-
-    internal fun attachLifecycleOwner(owner: LifecycleOwner) {
-        watchData.forEach { (t, u) ->
-            t.observe(owner, u as Observer<in Any>)
-        }
-        owner.lifecycle.addObserver(object : LifecycleEventObserver {
-            override fun onStateChanged(source: LifecycleOwner, event: Lifecycle.Event) {
-                when (event) {
-                    Lifecycle.Event.ON_PAUSE -> {
-                        if ((source is Activity && source.isFinishing) || (source is Fragment && source.activity != null && source.requireActivity().isFinishing)) {
-                            clearWatchData()
-                        }
-                    }
-                    Lifecycle.Event.ON_DESTROY -> {
-                        clearWatchData()
-                    }
-                    else -> {}
-                }
-            }
-
-        })
-    }
-
-    private fun clearWatchData() {
-        if (watchData.isEmpty()) {
-            return
-        }
-        watchData.forEach { (t, u) ->
-            t.removeObserver(u as Observer<in Any>)
-        }
-        watchData.clear()
-    }
+    return this
 }
