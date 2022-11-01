efaa4f6968547a8b9422b4557f8e9648fc545c92
fanqieVip <42194904+fanqieVip@users.noreply.github.com>
2022-11-01 16:43:34 +0800

    Update WatchKtx.kt
    移除原来不安全的watch方法（可能造成内存泄漏），新增WatchLiveData类，用于处理LiveData的数据观察

--- lib_base/src/main/java/com/frame/basic/base/ktx/WatchKtx.kt
+++ lib_base/src/main/java/com/frame/basic/base/ktx/WatchKtx.kt
@@ -1,65 +1,72 @@
 package com.frame.basic.base.ktx
 
-import androidx.lifecycle.MutableLiveData
-import com.frame.basic.base.mvvm.vm.CoreVM
+import android.app.Activity
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.*
 
 /**
- * @Description:    LiveData观察者，可自定义观察对象，及时更新自己
- * @Author:         fanj
- * @CreateDate:     2022/6/10 15:01
- * @Version:
+ * LiveData观察者，可自定义观察对象，及时更新自己
  */
-inline fun <reified T, DATA> Lazy<MutableLiveData<T>>.watch(
-    targetVM: CoreVM,
-    vmWatch: VMWatchOwner<T, DATA>
-): Lazy<MutableLiveData<T>> {
-    vmWatch.observe(targetVM, this.value)
-    return this
-}
+class WatchLiveData<T>(initData: T? = null) : MutableLiveData<T>(initData) {
+    private val watchData by lazy { HashMap<MutableLiveData<*>, Observer<*>>() }
 
-inline fun <reified T, DATA> Lazy<MutableLiveData<T>>.watch(
-    targetVM: CoreVM,
-    data: MutableLiveData<DATA>?,
-    crossinline onChange: (data: DATA?, self: MutableLiveData<T>) -> Unit
-): Lazy<MutableLiveData<T>> {
-    data?.let { liveData ->
-        targetVM.safeObserveForever(liveData) {
-            onChange(it as? DATA, this.value)
+    /**
+     * 该方法会绑定生命周期自动绑定和解绑
+     */
+    fun <D> watch(
+        data: MutableLiveData<D>?,
+        onChange: (data: D?, self: WatchLiveData<T>) -> Unit
+    ): WatchLiveData<T> {
+        data?.let {
+            watchData.put(it, Observer<D> { t ->
+                onChange.invoke(t, this@WatchLiveData) }
+            )
         }
+        return this
     }
-    return this
-}
 
-inline fun <reified T, DATA> MutableLiveData<T>.watch(
-    targetVM: CoreVM,
-    vmWatch: VMWatchOwner<T, DATA>
-): MutableLiveData<T> {
-    vmWatch.observe(targetVM, this)
-    return this
-}
-
-inline fun <reified T, DATA> MutableLiveData<T>.watch(
-    targetVM: CoreVM,
-    data: MutableLiveData<DATA>?,
-    crossinline onChange: (data: DATA?, self: MutableLiveData<T>) -> Unit
-): MutableLiveData<T> {
-    data?.let { liveData ->
-        targetVM.safeObserveForever(liveData) {
-            onChange(it as? DATA, this)
+    /**
+     * 该方法不绑定生命周期，容易造成内存泄漏，谨慎使用
+     */
+    fun <D> watchForever(
+        data: MutableLiveData<D>?,
+        onChange: (data: D?, self: WatchLiveData<T>) -> Unit
+    ): WatchLiveData<T> {
+        data?.observeForever {
+            onChange(it, this)
         }
+        return this
     }
-    return this
-}
 
-interface VMWatchOwner<W, T> {
-    fun observe(coreVM: CoreVM, to: MutableLiveData<W>) {
-        watch()?.let { watcher ->
-            coreVM.safeObserveForever(watcher) {
-                onChange(it as? T, to)
+    internal fun attachLifecycleOwner(owner: LifecycleOwner) {
+        watchData.forEach { (t, u) ->
+            t.observe(owner, u as Observer<in Any>)
+        }
+        owner.lifecycle.addObserver(object : LifecycleEventObserver {
+            override fun onStateChanged(source: LifecycleOwner, event: Lifecycle.Event) {
+                when (event) {
+                    Lifecycle.Event.ON_PAUSE -> {
+                        if ((source is Activity && source.isFinishing) || (source is Fragment && source.activity != null && source.requireActivity().isFinishing)) {
+                            clearWatchData()
+                        }
+                    }
+                    Lifecycle.Event.ON_DESTROY -> {
+                        clearWatchData()
+                    }
+                    else -> {}
+                }
             }
-        }
+
+        })
     }
 
-    fun watch(): MutableLiveData<T>?
-    fun onChange(data: T?, to: MutableLiveData<W>)
+    private fun clearWatchData() {
+        if (watchData.isEmpty()) {
+            return
+        }
+        watchData.forEach { (t, u) ->
+            t.removeObserver(u as Observer<in Any>)
+        }
+        watchData.clear()
+    }
 }
