cf8989c658ddc16dc9c64dc26130dbe3fa7bbe3e
fanqieVip <42194904+fanqieVip@users.noreply.github.com>
2022-11-02 10:53:20 +0800

    Update WatchKtx.kt
    重构Watch语法

--- lib_base/src/main/java/com/frame/basic/base/ktx/WatchKtx.kt
+++ lib_base/src/main/java/com/frame/basic/base/ktx/WatchKtx.kt
@@ -2,15 +2,14 @@ package com.frame.basic.base.ktx
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.Observer
-import com.frame.basic.base.mvvm.vm.BaseVM
-import kotlin.collections.ArrayList
+import com.frame.basic.base.mvvm.vm.CoreVM
 import kotlin.collections.set
 
 /**
  * 绑定生命周期的Watch方法
  */
 fun <T, DATA> MutableLiveData<T>.watch(
-    vm: BaseVM,
+    vm: CoreVM,
     data: MutableLiveData<DATA>?,
     onChange: (data: DATA?, self: MutableLiveData<T>) -> Unit
 ): MutableLiveData<T> {
@@ -34,6 +33,37 @@ fun <T, DATA> MutableLiveData<T>.watchForever(
 ): MutableLiveData<T> {
     data?.observeForever {
         onChange(it, this)
+    }
+    return this
+}
+/**
+ * 绑定生命周期的Watch方法
+ */
+fun <T, DATA> Lazy<MutableLiveData<T>>.watch(
+    vm: CoreVM,
+    data: MutableLiveData<DATA>?,
+    onChange: (data: DATA?, self: MutableLiveData<T>) -> Unit
+): Lazy<MutableLiveData<T>> {
+    data?.let {
+        var watchData = vm.watchLiveDataMap[it]
+        if (watchData == null) {
+            watchData = ArrayList<Observer<*>>()
+        }
+        watchData.add(Observer<DATA> { t -> onChange.invoke(t, this.value) })
+        vm.watchLiveDataMap[it] = watchData
+    }
+    return this
+}
+
+/**
+ * 不绑定生命周期的Watch方法，有内存泄漏风险，谨慎使用
+ */
+fun <T, DATA> Lazy<MutableLiveData<T>>.watchForever(
+    data: MutableLiveData<DATA>?,
+    onChange: (data: DATA?, self: MutableLiveData<T>) -> Unit
+): Lazy<MutableLiveData<T>> {
+    data?.observeForever {
+        onChange(it, this.value)
     }
     return this
 }
