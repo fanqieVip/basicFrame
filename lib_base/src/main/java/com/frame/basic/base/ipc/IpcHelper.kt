03e7cd36c510f5a944a460062686b915bd6e7b57
fanqieVip <42194904+fanqieVip@users.noreply.github.com>
2022-11-11 15:37:57 +0800

    Update IpcHelper.kt
    IPC框架减少IMessager的创建开销，并新增生命周期绑定功能，避免回调时的内存泄漏

--- lib_base/src/main/java/com/frame/basic/base/ipc/IpcHelper.kt
+++ lib_base/src/main/java/com/frame/basic/base/ipc/IpcHelper.kt
@@ -1,16 +1,22 @@
 package com.frame.basic.base.ipc
 
+import android.app.Activity
 import android.content.ComponentName
 import android.content.Context
 import android.content.Intent
 import android.content.ServiceConnection
 import android.os.*
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleEventObserver
+import androidx.lifecycle.LifecycleOwner
 import com.frame.basic.base.BaseApplication
 
 object IpcHelper {
     internal const val MSG_FROM_SERVER = 1
     internal const val MSG_FROM_CLIENT = 2
     private val connections by lazy { HashMap<Class<out IpcService>, MyServiceConnection>() }
+    private val messengers by lazy { HashMap<Class<out IpcService>, Messenger>() }
 
     fun register(vararg remotes: Class<out IpcService>) {
         val app = BaseApplication.application
@@ -33,20 +39,21 @@ object IpcHelper {
     @JvmStatic
     fun <T> sendMsg(
         remote: Class<out IpcService>,
+        owner: LifecycleOwner?,
         bundle: Bundle,
         callBlock: CallBlock<T>?
     ) {
         connections[remote]?.binder?.let { binder ->
-            val messenger = Messenger(binder)
+            var messenger = messengers[remote]
+            if (messenger == null || messenger.binder != binder) {
+                messenger = Messenger(binder)
+                messengers[remote] = messenger
+            }
             val message = Message.obtain(null, MSG_FROM_CLIENT).apply {
                 data = bundle
-                replyTo = Messenger(object : Handler(Looper.getMainLooper()) {
-                    override fun handleMessage(msg: Message) {
-                        if (msg.what == MSG_FROM_SERVER) {
-                            callBlock?.success(msg.data.getSerializable("result") as? T)
-                        }
-                    }
-                })
+                if (callBlock != null){
+                    replyTo = Messenger(LifecycleHandler(owner, Looper.getMainLooper(), callBlock))
+                }
             }
             try {
                 messenger.send(message)
@@ -54,7 +61,6 @@ object IpcHelper {
                 e.printStackTrace()
                 callBlock?.error(e.localizedMessage)
             }
-
         }
     }
 
@@ -67,6 +73,42 @@ object IpcHelper {
         override fun onServiceDisconnected(name: ComponentName?) {
             binder = null
         }
+    }
 
+    internal class LifecycleHandler<T>(
+        private var owner: LifecycleOwner?,
+        looper: Looper,
+        private var callBlock: CallBlock<T>?
+    ) : Handler(looper), LifecycleEventObserver {
+        init {
+            owner?.lifecycle?.addObserver(this)
+        }
+
+        override fun onStateChanged(source: LifecycleOwner, event: Lifecycle.Event) {
+            when (event) {
+                Lifecycle.Event.ON_PAUSE -> {
+                    if ((source is Activity && source.isFinishing) || (source is Fragment && source.activity != null && source.requireActivity().isFinishing)) {
+                        recycle()
+                    }
+                }
+                Lifecycle.Event.ON_DESTROY -> {
+                    recycle()
+                }
+                else -> {}
+            }
+        }
+
+        override fun handleMessage(msg: Message) {
+            if (msg.what == MSG_FROM_SERVER) {
+                callBlock?.success(msg.data.getSerializable("result") as? T)
+                recycle()
+            }
+        }
+
+        private fun recycle(){
+            callBlock = null
+            owner?.lifecycle?.removeObserver(this)
+            owner = null
+        }
     }
 }
