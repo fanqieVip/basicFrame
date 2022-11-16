8087b0a0052dd2eb23eb11d3cc84751aece2789e
fanqieVip <42194904+fanqieVip@users.noreply.github.com>
2022-11-16 11:50:57 +0800

    Update LiveDataKtx.kt
    修复observeForever引发的DisplayStatus.ONPAUSE状态LiveData不响应的bug

--- lib_base/src/main/java/com/frame/basic/base/ktx/LiveDataKtx.kt
+++ lib_base/src/main/java/com/frame/basic/base/ktx/LiveDataKtx.kt
@@ -1,8 +1,6 @@
 package com.frame.basic.base.ktx
 
-import android.app.Activity
 import androidx.annotation.NonNull
-import androidx.fragment.app.Fragment
 import androidx.lifecycle.*
 
 /**
@@ -15,7 +13,7 @@ fun <T> LiveData<T>.observeForever(
     observeForever(observer)
     owner.lifecycle.addObserver(object : LifecycleEventObserver {
         override fun onStateChanged(source: LifecycleOwner, event: Lifecycle.Event) {
-            if (event == Lifecycle.Event.ON_DESTROY || (event == Lifecycle.Event.ON_PAUSE  && ((source is Activity && source.isFinishing) || (source is Fragment && source.activity != null && source.requireActivity().isFinishing)))) {
+            if (event == Lifecycle.Event.ON_DESTROY) {
                 removeObserver(observer)
             }
         }
