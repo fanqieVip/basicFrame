00a265c611f210cbf64ba5b444001a9375ed5df6
fanqieVip <42194904+fanqieVip@users.noreply.github.com>
2022-11-23 15:23:38 +0800

    Update LiveDataKtx.kt
    新增绑定真正生命周期的LiveData.observe方法，用于解决多层嵌套fragment时，由于安卓LiveData原生生命周期不准确可能会造成同时触发很多响应的问题

--- lib_base/src/main/java/com/frame/basic/base/ktx/LiveDataKtx.kt
+++ lib_base/src/main/java/com/frame/basic/base/ktx/LiveDataKtx.kt
@@ -2,6 +2,7 @@ package com.frame.basic.base.ktx
 
 import androidx.annotation.NonNull
 import androidx.lifecycle.*
+import com.frame.basic.base.mvvm.c.DisplayStatus
 
 /**
  * 不感知声明周期，实时响应，但会伴随页面销毁而销毁
@@ -16,6 +17,32 @@ fun <T> LiveData<T>.observeForever(
             if (event == Lifecycle.Event.ON_DESTROY) {
                 removeObserver(observer)
             }
+        }
+    })
+}
+
+/**
+ * 绑定真正的页面生命周期，在页面真正显示时才触发
+ * @param displayStatus 必须是页面主VM中的displayStatus
+ */
+fun <T> LiveData<T>.observe(@NonNull displayStatus: LiveData<DisplayStatus>, @NonNull owner: LifecycleOwner, @NonNull observer: Observer<in T>){
+    val realLiveData = MutableLiveData<T>()
+    realLiveData.observe(owner, observer)
+    var stagingData: T? = null
+    observe(owner, Observer {
+        stagingData = null
+        if (displayStatus.value == DisplayStatus.SHOWING){
+            realLiveData.value = it
+        }else{
+            stagingData = it
+        }
+    })
+    displayStatus.observe(owner, Observer {
+        if (it == DisplayStatus.SHOWING){
+            stagingData?.let { data ->
+                realLiveData.value = data
+            }
+            stagingData = null
         }
     })
 }
