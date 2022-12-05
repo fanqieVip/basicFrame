42bc7f23ddf229875c561eebb28b7b3171919bb5
fanqieVip <42194904+fanqieVip@users.noreply.github.com>
2022-12-05 10:33:44 +0800

    Create ImageViewKtx.kt
    修复glide加载图片时可能会抛出“IllegalArgumentException  You cannot start a load for a destroyed activity at com.bumptech.glide.manager.RequestManagerRetriever.assertNotDestroyed”异常的bug

--- lib_base/src/main/java/com/frame/basic/base/ktx/ImageViewKtx.kt
+++ lib_base/src/main/java/com/frame/basic/base/ktx/ImageViewKtx.kt
@@ -1,8 +1,24 @@
 package com.frame.basic.base.ktx
 
+import android.app.Activity
 import android.widget.ImageView
 import com.bumptech.glide.Glide
+import dagger.hilt.android.internal.managers.ViewComponentManager
 
-fun ImageView.loadImage(path: String?){
+fun ImageView.loadImage(path: String?) {
+    val context = this.context ?: return
+    when (context) {
+        is Activity -> {
+            if (context.isDestroyed || context.isFinishing) {
+                return
+            }
+        }
+        is ViewComponentManager.FragmentContextWrapper -> {
+            val ctx = context.baseContext ?: return
+            if (ctx is Activity && (ctx.isDestroyed || ctx.isFinishing)) {
+                return
+            }
+        }
+    }
     Glide.with(context).load(path).into(this)
 }
